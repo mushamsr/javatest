5a0ca793afc66c59acb21e1f0b1a04d7963fbacb
Sudhakar <Sudhakar@Musham-PC>
2018-02-03 18:39:21 -0500

    First attempt at using GIT locally

---
+++ MyFirstProject/src/MyFirstJavaClass.java
@@ -0,0 +1,19 @@
+/**
+ * what is this about?????
+ */
+
+/**
+ * @author Sudhakar
+ *
+ */
+public class MyFirstJavaClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Chocolate, royalties, sleep");
+
+	}
+
+}
