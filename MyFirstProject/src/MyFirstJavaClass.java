b827857fd2139ffdf46f72507769a8288f24a9b0
Sudhakar <Sudhakar@Musham-PC>
2018-02-04 12:16:11 -0500

    Changes args variable to hmm

--- MyFirstProject/src/MyFirstJavaClass.java
+++ MyFirstProject/src/MyFirstJavaClass.java
@@ -11,7 +11,7 @@ public class MyFirstJavaClass {
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] hmm) {
 		System.out.println("Chocolate, royalties, sleep");
 
 	}
